1f1d98b7d30b6f071929ca4be69c77e1b9e8b66b
Adrian <felixadrianhdez@gmail.com>
2025-04-07 00:51:55 -0600

    Preparing Data

---
+++ Some Tests.py
@@ -0,0 +1,39 @@
+from scipy.stats import pointbiserialr
+import pandas as pd
+
+# 1.1 - Load the dataset from the CSV file  
+df = pd.read_csv("train.csv")
+
+
+
+# 1.2 - Check for missing values in each column  
+print(df.isnull().sum())
+
+# 1.3 - Remove the 'Cabin' column due to excessive missing data (687 out of 891 entries)  
+df = df.drop('Cabin', axis=1)
+
+# 1.4 - Verify remaining missing values after column removal  
+print(df.isnull().sum())
+
+# 1.5 - Drop the two rows with missing 'Embarked' values  
+df = df.dropna(subset=['Embarked'])
+
+# 1.6 - Verifying empty values one more time
+print(df.isnull().sum())
+
+
+df=df.dropna(subset=['Age'])
+print(df.isnull().sum())
+
+
+
+binaryVar = df['Survived']
+
+contVar = df['Age']
+
+
+
+corr, pValue = pointbiserialr(binaryVar, contVar)
+
+print(corr)
+print(pValue)
